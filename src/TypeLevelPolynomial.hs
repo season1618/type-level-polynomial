bbba84935c06afd20c1d42a25b0a852f212e0456
season1618 <seasonminority314@gmail.com>
2024-02-02 18:29:09 +0900

    add of polynomial.

--- src/TypeLevelPolynomial.hs
+++ src/TypeLevelPolynomial.hs
@@ -1,7 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module TypeLevelPolynomial (
+    List(Nil, Cons),
     Polynomial(Polynomial),
 ) where
 
 import GHC.TypeLits
 
-data Polynomial = Polynomial [Symbol]
+data List a = Nil | Cons a (List a)
+
+type family Merge (x :: List a) (y :: List a) :: List a where
+    Merge Nil y = y
+    Merge x Nil = x
+    Merge (Cons x xs) (Cons y ys) = MergeIf (CmpSymbol x y) (Cons x (Merge xs (Cons y ys))) (Cons y (Merge (Cons x xs) ys))
+
+type family MergeIf (c :: Ordering) (t :: List a) (e :: List a) :: List a where
+    MergeIf EQ t e = t
+    MergeIf LT t e = t
+    MergeIf GT t e = e
+
+data Polynomial = Polynomial (List Symbol)
+
+type family Add (x :: Polynomial) (y :: Polynomial) :: Polynomial where
+    Add ('Polynomial x) ('Polynomial y) = 'Polynomial (Merge x y)
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,6 @@ import Vector
 
 main :: IO ()
 main = do
-    let x = Vector [1, 2, 3] :: Vector ('Polynomial ["0", "a"]) Int
-    let y = Vector [1, 2, 3] :: Vector ('Polynomial ["0", "a"]) Int
+    let x = Vector [1, 2, 3] :: Vector ('Polynomial ('Cons "a" 'Nil)) Int
+    let y = Vector [1, 2, 3] :: Vector ('Polynomial ('Cons "a" 'Nil)) Int
     print $ x == y
