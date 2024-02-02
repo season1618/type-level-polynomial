6b68e828ebcc8529d0159055cdfe2eb31fc88865
season1618 <seasonminority314@gmail.com>
2024-02-02 18:35:32 +0900

    add of vector.

--- src/Vector.hs
+++ src/Vector.hs
@@ -6,3 +6,6 @@ import TypeLevelPolynomial
 
 data Vector (n :: Polynomial) a = Vector [a]
     deriving (Eq, Show)
+
+add :: Num a => Vector n a -> Vector n a -> Vector n a
+add (Vector x) (Vector y) = Vector (zipWith (+) x y)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,5 +6,5 @@ import Vector
 main :: IO ()
 main = do
     let x = Vector [1, 2, 3] :: Vector ('Polynomial ('Cons "a" 'Nil)) Int
-    let y = Vector [1, 2, 3] :: Vector ('Polynomial ('Cons "a" 'Nil)) Int
-    print $ x == y
+    let y = Vector [2, 3, 4] :: Vector ('Polynomial ('Cons "a" 'Nil)) Int
+    print $ add x y
