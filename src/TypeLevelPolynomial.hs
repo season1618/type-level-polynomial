5f45759e4d3dee2829073f3c6550299dcf5b7f9f
season1618 <seasonminority314@gmail.com>
2024-02-03 19:24:41 +0900

    1-dim polynomial.

--- src/TypeLevelPolynomial.hs
+++ src/TypeLevelPolynomial.hs
@@ -6,23 +6,41 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module TypeLevelPolynomial (
+    Zahl(Pos, Zero, Neg),
     Polynomial(Polynomial),
     Add,
 ) where
 
 import GHC.TypeLits
 
-type family Merge (x :: [a]) (y :: [a]) :: [a] where
-    Merge '[] y = y
-    Merge x '[] = x
-    Merge (x:xs) (y:ys) = MergeIf (CmpSymbol x y) (x : (Merge xs (y:ys))) (y : (Merge (x:xs) ys))
+data Zahl = Pos Nat
+          | Zero
+          | Neg Nat
 
-type family MergeIf (c :: Ordering) (t :: [a]) (e :: [a]) :: [a] where
-    MergeIf EQ t e = t
-    MergeIf LT t e = t
-    MergeIf GT t e = e
+type family AddZahl (x :: Zahl) (y :: Zahl) :: Zahl where
+    AddZahl x 'Zero = x
+    AddZahl 'Zero x = x
+    AddZahl ('Pos x) ('Pos y) = 'Pos (x + y)
+    AddZahl ('Pos x) ('Neg y) = CmpAddZahl (CmpNat x y) x y
+    AddZahl ('Neg x) ('Pos y) = CmpAddZahl (CmpNat y x) y x
+    AddZahl ('Neg x) ('Neg y) = 'Neg (x + y)
 
-data Polynomial = Polynomial [Symbol]
+type family CmpAddZahl (c :: Ordering) (x :: Nat) (y :: Nat) :: Zahl where
+    CmpAddZahl 'LT x y = 'Neg (y - x)
+    CmpAddZahl 'EQ x y = 'Zero
+    CmpAddZahl 'GT x y = 'Pos (x - y)
+
+data Polynomial = Polynomial [(Symbol, Zahl)]
 
 type family Add (x :: Polynomial) (y :: Polynomial) :: Polynomial where
-    Add ('Polynomial x) ('Polynomial y) = 'Polynomial (Merge x y)
+    Add ('Polynomial x) ('Polynomial y) = 'Polynomial (AddList x y)
+
+type family AddList (x :: [(Symbol, Zahl)]) (y :: [(Symbol, Zahl)]) :: [(Symbol, Zahl)] where
+    AddList '[] y = y
+    AddList x '[] = x
+    AddList ('(v1, k1) : xs) ('(v2, k2) : ys) = CmpAddList (CmpSymbol v1 v2) ('(v1, k1) : xs) ('(v2, k2) : ys)
+
+type family CmpAddList (c :: Ordering) (x :: [(Symbol, Zahl)]) (y :: [(Symbol, Zahl)]) :: [(Symbol, Zahl)] where
+    CmpAddList 'LT (x:xs) (y:ys) = x : AddList xs (y:ys)
+    CmpAddList 'EQ ('(v1, k1) : xs) ('(v2, k2) : ys) = '(v1, AddZahl k1 k2) : AddList xs ys
+    CmpAddList 'GT (x:xs) (y:ys) = y : AddList (x:xs) ys
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,16 +6,16 @@ import Matrix
 
 main :: IO ()
 main = do
-    let x = Vector [1, 2, 3] :: Vector ('Polynomial '["a"]) Float
-    let y = Vector [2, 3, 4] :: Vector ('Polynomial '["b"]) Float
+    let x = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("a", 'Pos 1) ]) Float
+    let y = Vector [2, 3, 4] :: Vector ('Polynomial '[ '("b", 'Pos 1) ]) Float
     print $ append x y == append y x
     print $ x + x
     print $ x - x
     print $ x `dot` x
     print $ norm x
 
-    let a = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial '["a"]) ('Polynomial '["b"]) Float
-    let b = Matrix [[1, 2], [2, 3], [3, 4]] :: Matrix ('Polynomial '["b"]) ('Polynomial '["a"]) Float
+    let a = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial '[ '("a", 'Pos 1) ]) ('Polynomial '[ '("b", 'Pos 1) ]) Float
+    let b = Matrix [[1, 2], [2, 3], [3, 4]] :: Matrix ('Polynomial '[ '("b", 'Pos 1) ]) ('Polynomial '[ '("a", 'Pos 1) ]) Float
     print $ appendRow a a
     print $ appendCol a a
     print $ a + a
