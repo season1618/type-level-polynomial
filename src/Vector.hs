c3ef0866831f49a9ae04fa79e70f07d11c8ec590
season1618 <seasonminority314@gmail.com>
2024-02-03 19:58:33 +0900

    uncons of vector.

--- src/Vector.hs
+++ src/Vector.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 
 module Vector where
@@ -9,6 +10,10 @@ data Vector (n :: Polynomial) a = Vector [a]
 
 append :: Vector n a -> Vector m a -> Vector (Add n m) a
 append (Vector x) (Vector y) = Vector (x ++ y)
+
+uncons :: Vector n a -> Maybe (a, Vector (Add n ('Polynomial '[ '("1", 'Neg 1) ])) a)
+uncons (Vector []) = Nothing
+uncons (Vector (x:xs)) = Just (x, Vector xs)
 
 instance Num a => Num (Vector n a) where
     (+) (Vector x) (Vector y) = Vector (zipWith (+) x y)
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,9 +7,11 @@ import Matrix
 main :: IO ()
 main = do
     let x = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("1", 'Pos 3) ]) Float
+    let Just (_, xs) = uncons x
     let y = Vector [4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 2) ]) Float
     let z = Vector [1, 2, 3, 4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 5)]) Float
     print $ append x y == z
+    print $ xs + y
 
     let x = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("a", 'Pos 1) ]) Float
     let y = Vector [2, 3, 4] :: Vector ('Polynomial '[ '("b", 'Pos 1) ]) Float
