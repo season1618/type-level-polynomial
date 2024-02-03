3f03b6658cfd448574a7ac12bf8ecd26f03cf36d
season1618 <seasonminority314@gmail.com>
2024-02-03 15:02:39 +0900

    replace List with [].

--- src/TypeLevelPolynomial.hs
+++ src/TypeLevelPolynomial.hs
@@ -2,29 +2,27 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module TypeLevelPolynomial (
-    List(Nil, Cons),
     Polynomial(Polynomial),
     Add,
 ) where
 
 import GHC.TypeLits
 
-data List a = Nil | Cons a (List a)
+type family Merge (x :: [a]) (y :: [a]) :: [a] where
+    Merge '[] y = y
+    Merge x '[] = x
+    Merge (x:xs) (y:ys) = MergeIf (CmpSymbol x y) (x : (Merge xs (y:ys))) (y : (Merge (x:xs) ys))
 
-type family Merge (x :: List a) (y :: List a) :: List a where
-    Merge Nil y = y
-    Merge x Nil = x
-    Merge (Cons x xs) (Cons y ys) = MergeIf (CmpSymbol x y) (Cons x (Merge xs (Cons y ys))) (Cons y (Merge (Cons x xs) ys))
-
-type family MergeIf (c :: Ordering) (t :: List a) (e :: List a) :: List a where
+type family MergeIf (c :: Ordering) (t :: [a]) (e :: [a]) :: [a] where
     MergeIf EQ t e = t
     MergeIf LT t e = t
     MergeIf GT t e = e
 
-data Polynomial = Polynomial (List Symbol)
+data Polynomial = Polynomial [Symbol]
 
 type family Add (x :: Polynomial) (y :: Polynomial) :: Polynomial where
     Add ('Polynomial x) ('Polynomial y) = 'Polynomial (Merge x y)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,16 +6,16 @@ import Matrix
 
 main :: IO ()
 main = do
-    let x = Vector [1, 2, 3] :: Vector ('Polynomial ('Cons "a" 'Nil)) Float
-    let y = Vector [2, 3, 4] :: Vector ('Polynomial ('Cons "b" 'Nil)) Float
+    let x = Vector [1, 2, 3] :: Vector ('Polynomial '["a"]) Float
+    let y = Vector [2, 3, 4] :: Vector ('Polynomial '["b"]) Float
     print $ append x y == append y x
     print $ x + x
     print $ x - x
     print $ x `dot` x
     print $ norm x
 
-    let a = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial ('Cons "a" 'Nil)) ('Polynomial ('Cons "b" 'Nil)) Float
-    let b = Matrix [[1, 2], [2, 3], [3, 4]] :: Matrix ('Polynomial ('Cons "b" 'Nil)) ('Polynomial ('Cons "a" 'Nil)) Float
+    let a = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial '["a"]) ('Polynomial '["b"]) Float
+    let b = Matrix [[1, 2], [2, 3], [3, 4]] :: Matrix ('Polynomial '["b"]) ('Polynomial '["a"]) Float
     print $ appendRow a a
     print $ appendCol a a
     print $ a + a
