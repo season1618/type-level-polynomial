f3bb89aedb7f8f9857d9d2cdf43c575e0e207cbd
season1618 <seasonminority314@gmail.com>
2024-02-03 20:10:03 +0900

    uncons of matrix.

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 
 module Matrix where
 
 import TypeLevelPolynomial
+import Vector
 
 data Matrix (n :: Polynomial) (m :: Polynomial) a = Matrix [[a]]
     deriving (Eq, Show)
@@ -12,6 +14,10 @@ appendRow (Matrix x) (Matrix y) = Matrix (x ++ y)
 
 appendCol :: Matrix n m1 a -> Matrix n m2 a -> Matrix n (Add m1 m2) a
 appendCol (Matrix x) (Matrix y) = Matrix [xi ++ yi | (xi, yi) <- zip x y]
+
+uncons :: Matrix m n a -> Maybe (Vector n a, Matrix (Add m ('Polynomial '[ '("1", 'Neg 1) ])) n a)
+uncons (Matrix []) = Nothing
+uncons (Matrix (x:xs)) = Just (Vector x, Matrix xs)
 
 instance Num a => Num (Matrix n m a) where
     (+) (Matrix x) (Matrix y) = Matrix (zipWith (zipWith (+)) x y)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE DataKinds #-}
 
 import TypeLevelPolynomial
-import Vector
-import Matrix
+import Vector as Vec
+import Matrix as Mat
 
 main :: IO ()
 main = do
     let x = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("1", 'Pos 3) ]) Float
-    let Just (_, xs) = uncons x
+    let Just (_, xs) = Vec.uncons x
     let y = Vector [4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 2) ]) Float
     let z = Vector [1, 2, 3, 4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 5)]) Float
     print $ append x y == z
@@ -23,6 +23,9 @@ main = do
 
     let a = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial '[ '("a", 'Pos 1) ]) ('Polynomial '[ '("b", 'Pos 1) ]) Float
     let b = Matrix [[1, 2], [2, 3], [3, 4]] :: Matrix ('Polynomial '[ '("b", 'Pos 1) ]) ('Polynomial '[ '("a", 'Pos 1) ]) Float
+    let Just (av, am) = Mat.uncons a
+    print $ y == av
+    print $ mul am b
     print $ appendRow a a
     print $ appendCol a a
     print $ a + a
