6c32435e8510827d5d196c7dcfd8543d9a68c852
season1618 <seasonminority314@gmail.com>
2024-02-07 10:34:01 +0900

    type synonym.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,13 +5,17 @@ import Vector as Vec
 import Matrix as Mat
 import Test.HUnit
 
+type A = 'Polynomial '[ '("a", 'Pos 1) ]
+type B = 'Polynomial '[ '("b", 'Pos 1) ]
+type N = 'Polynomial '[ '("n", 'Pos 1) ]
+
 main :: IO Counts
 main = do
     let v1 = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("1", 'Pos 3) ]) Float
     let v2 = Vector [4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 2) ]) Float
     let v3 = Vector [1, 2, 3, 4, 5] :: Vector ('Polynomial '[ '("1", 'Pos 5)]) Float
-    let v4 = Vector [1, 2, 3] :: Vector ('Polynomial '[ '("a", 'Pos 1) ]) Float
-    let v5 = Vector [2, 3, 4] :: Vector ('Polynomial '[ '("b", 'Pos 1) ]) Float
+    let v4 = Vector [1, 2, 3] :: Vector A Float
+    let v5 = Vector [2, 3, 4] :: Vector B Float
 
     let vectorTest = TestList [ TestCase $ assertEqual "append" (append v1 v2) v3
                               , TestCase $ assertEqual "uncons, scalar" (let Just (x, xs) = Vec.uncons v4 in append (scalar x) xs) v4
@@ -20,9 +24,9 @@ main = do
                               ]
     runTestTT vectorTest
 
-    let m1 = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix ('Polynomial '[ '("a", 'Pos 1) ]) ('Polynomial '[ '("b", 'Pos 1) ]) Float
+    let m1 = Matrix [[1, 2, 3], [2, 3, 4]] :: Matrix A B Float
     let m2 = transpose m1
-    let m3 = Matrix [ [8, 16, 24, 32], [2,  7, 12, 17], [6, 17, 32, 59], [7, 22, 46, 105] ] :: Matrix ('Polynomial '[ '("n", 'Pos 1) ]) ('Polynomial '[ '("n", 'Pos 1) ]) Float
+    let m3 = Matrix [ [8, 16, 24, 32], [2,  7, 12, 17], [6, 17, 32, 59], [7, 22, 46, 105] ] :: Matrix N N Float
 
     let matrixTest = TestList [ TestCase $ assertEqual "append-row" (appendRow m1 m1) (Matrix [[1, 2, 3], [2, 3, 4], [1, 2, 3], [2, 3, 4]])
                               , TestCase $ assertEqual "append-col" (appendCol m1 m1) (Matrix [[1, 2, 3, 1, 2, 3], [2, 3, 4, 2, 3, 4]])
